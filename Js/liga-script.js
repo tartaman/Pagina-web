f59653d2c4c1fb13fd778b83a62cb46b6a25ada3
tartaman <70653340+tartaman@users.noreply.github.com>
2020-11-19 02:24:36 -0600

    casi termino la vista liga kanto
    Casi termino la vista liga kanto con todos los líderes y el alto mando

---
+++ Js/liga-script.js
@@ -0,0 +1,21 @@
+function aparecelorelei(){
+    document.getElementById('lider').innerHTML = '<h1 class="lorelei2">Lorelei</h1>'
+    document.getElementById('tipo').innerHTML = '<p class="tipo" style="background-color: #58C8E0;">Hielo</p>'
+    document.getElementById('lidertexto').innerHTML = '<p class="textoqueaparece">¡Bienvenido a la LIGA POKéMON!¡Yo soy LORELEI del ALTO MANDO!¡¡Nadie me supera con los pokémon que congelan!!¡Los movimientos congelantes son muy potentes!¡Tus POkéMON estarán a mi merced cuando los congele!¡Ja! ¡Ja! ¡Ja! ¿Estás listo?'
+} 
+function aparecebruno(){
+    document.getElementById('lider').innerHTML = '<h1 class="bruno2">BRUNO</h1>'
+    document.getElementById('tipo').innerHTML = '<p class="tipo" style="background-color: #A05038;">Lucha</p>'
+    document.getElementById('lidertexto').innerHTML ='¡Yo soy BRUNO del ALTO MANDO!¡Con un riguroso entrenamiento, la gente y los POKéMON pueden fortalecerse!¡Yo he entrenado a mis POKéMON con pesas!¡Os machacaremos con nuestra gran fuerza!'
+}
+function apareceagatha(){
+    document.getElementById('lider').innerHTML = '<h1 class="agatha2">Agatha</h1>'
+    document.getElementById('tipo').innerHTML = '<p class="tipo" style="background-color: #6060B0;">Fantasma</p>'
+    document.getElementById('lidertexto').innerHTML = '¡Yo soy AGATHA del ALTO MANDO!¡El PROF. OAK parece estar muy interesado en ti!¡Hace tiempo ese viejo inútil era fuerte y apuesto! ¡Pero eso fue hace décadas!¡Ahora sólo juguetea con su POKéDEX! ¡Pero está equivocado! ¡Los POKéMON son para luchar!¡Yo te enseñaré cómo lucha un gran entrenador!'
+
+}
+function aparecelance(){
+    document.getElementById('lider').innerHTML = '<h1 class="lance2">Lance</h1>'
+    document.getElementById('tipo').innerHTML = '<p class="tipo" style="background-color: #7038F8;">Dragón</p>'
+    document.getElementById('lidertexto').innerHTML = '¡Ah! ¡He oído hablar de ti!¡Dirijo al ALTO MANDO! ¡Puedes llamarme LANCE, el entrenador de dragones!¡Los dragones son míticos POKéMON!¡Son difíciles de capturar y entrenar pero sus poderes son muy superiores!¡Son casi invencibles!Bien, ¿preparado para perder?¡Tu desafío en la LIGA termina conmigo!'
+}
