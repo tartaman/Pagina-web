a6a87295df34b4287c94d9a558a2b8b2942c255b
tartaman <70653340+tartaman@users.noreply.github.com>
2020-11-20 16:44:19 -0600

    Penultimo cambio
    termine mi vista final, falta hacerle cambios menores a todo

--- Js/estrategias.js
+++ Js/estrategias.js
@@ -1,6 +1,26 @@
 function charmander(){
-    document.getElementById('escogiste').innerHTML = '¡Haz escogido a Charmander!, Busca estos pokémones, y ,¡con este equipo la tendrás más fácil!'
+    document.getElementById('escogistechar').innerHTML = '¡Haz escogido a Charmander!, Busca estos pokémones, y ,¡con este equipo la tendrás más fácil!'
 var a = document.getElementById('charmander');
+
+    if (a.style.display === "none") {
+        a.style.display = "block";
+    } else {
+        a.style.display = "none";
+    }
+}
+function bulbasaur(){
+    document.getElementById('escogistebulba').innerHTML = '¡Haz escogido a Bulbasaur!, Busca estos pokémones, y ,¡con este equipo la tendrás más fácil!'
+var a = document.getElementById('bulbasaur');
+
+    if (a.style.display === "none") {
+        a.style.display = "block";
+    } else {
+        a.style.display = "none";
+    }
+}
+function squirtle(){
+    document.getElementById('escogistesquir').innerHTML = '¡Haz escogido a Bulbasaur!, Busca estos pokémones, y ,¡con este equipo la tendrás más fácil!'
+var a = document.getElementById('squirtle');
 
     if (a.style.display === "none") {
         a.style.display = "block";
