3d257a45d45e55415b568bcbcf48cdf22b562eec
tartaman <70653340+tartaman@users.noreply.github.com>
2020-11-20 14:26:59 -0600

    ahora si ya gracias
    ya está profe

--- Js/scriptmapa.js
+++ Js/scriptmapa.js
@@ -20,15 +20,12 @@ function enseñarSlides(n) {
     }
     for (i = 0; i < circulos.length; i++){
         circulos[i].className = circulos[i].className.replace("active", "");
-<<<<<<< HEAD
     
     
     }
-    slides[indexSlides-1].className = "block";
-    circulos[indexSlides-1].className += " active";
-=======
-    }
-slides[indexSlides-1].style.display = "block";
+    slides[indexSlides-1].style.display = "block";
 circulos[indexSlides-1].className += " active";
->>>>>>> parent of a2f980f... arreglé mi vista de mapa liga
-}
+
+    }
+
+
